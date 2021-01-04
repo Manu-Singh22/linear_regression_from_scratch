e08612b5d7037dadf020cfa9257f08310c198681
Manu-Singh22 <64776771+Manu-Singh22@users.noreply.github.com>
2021-01-04 08:58:32 -0800

    Add files via upload
    This file consist of codes of Linear Regression from scratch.

---
+++ linear_reg.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import numpy as np
+class LinearRegression:
+    def __init__(self,lr=0.001,n_iters=1000):
+        self.lr=lr
+        self.n_iters=n_iters
+        self.weights=None
+        self.bias=None
+        
+    def fit(self,X,y):
+        #init paramerters
+        n_samples,n_features=X.shape
+        self.weights=np.zeros(n_features)
+        self.bias=0
+        
+        
+        for _ in range(self.n_iters):
+            y_predicted=np.dot(X,self.weights) + self.bias
+            
+            dw=2*(1/n_samples)*np.dot(X.T,(y_predicted-y))
+            
+            db=2*(1/n_samples)*np.sum(y_predicted-y)
+            
+            #weight update
+            
+            self.weights=self.weights-self.lr*dw
+            
+            self.bias=self.bias-self.lr*db
+            
+            
+    def predict(self,X):
+        y_predicted=np.dot(X,self.weights) + self.bias
+        return y_predicted
+        
+        
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
